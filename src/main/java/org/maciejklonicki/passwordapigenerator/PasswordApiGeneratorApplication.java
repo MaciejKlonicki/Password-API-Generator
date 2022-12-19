eb14b025fb3b7025157e773d46f7b5840043e860
Maciek <maciek.klonicki@gmail.com>
2022-12-19 15:32:52 +0100

    first commit

---
+++ src/main/java/org/maciejklonicki/passwordapigenerator/PasswordApiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.maciejklonicki.passwordapigenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordApiGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordApiGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/maciejklonicki/passwordapigenerator/PasswordApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maciejklonicki.passwordapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordApiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
